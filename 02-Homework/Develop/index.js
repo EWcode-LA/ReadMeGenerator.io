368aa78d88e5ac1d294f455b57cfe629e9633923
EWcode-LA <seth.miller87@proton.mail>
2020-12-09 18:35:06 -0800

    Changes

--- 02-Homework/Develop/index.js
+++ 02-Homework/Develop/index.js
@@ -1,4 +1,11 @@
 // array of questions for user
+//What is the title of your project?
+//What is the description of your description, installation instructions, usage information, contribution guidelines, and test instructions
+//Enter License for your project
+//Enter your Github username
+//Enter your E-mail address
+//Make links in readme clickable
+
 const questions = [
 
 ];
